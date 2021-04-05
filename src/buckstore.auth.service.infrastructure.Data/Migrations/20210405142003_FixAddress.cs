5707b7f5be184182ca5c9cd53b9e36a893ce6f31
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-04-05 11:23:39 -0300

    chore

---
+++ src/buckstore.auth.service.infrastructure.Data/Migrations/20210405142003_FixAddress.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace buckstore.auth.service.infrastructure.Data.Migrations
+{
+    public partial class FixAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address__city",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address__district",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address__state",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address__street",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address__zipCode",
+                table: "User",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address__city",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Address__district",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Address__state",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Address__street",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Address__zipCode",
+                table: "User");
+        }
+    }
+}
