38100622ad90d059385ac9b04fb3af14b7bbf672
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-06-14 12:03:57 -0300

    feat: add new method on repository to find user by id

--- src/buckstore.auth.service.infrastructure.Data/Repositories/UserRepository/UserRepository.cs
+++ src/buckstore.auth.service.infrastructure.Data/Repositories/UserRepository/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using buckstore.auth.service.domain.Aggregates.UserAggregate;
 using buckstore.auth.service.infrastructure.Data.Context;
 using Microsoft.EntityFrameworkCore;
@@ -13,8 +14,12 @@ namespace buckstore.auth.service.infrastructure.Data.Repositories.UserRepository
 
         public async Task<User> FindUserByEmail(string email)
         {
-            var merda = await _dbSet.FirstOrDefaultAsync(user => user.Email == email);
-            return merda;
+            return await _dbSet.FirstOrDefaultAsync(user => user.Email == email);
+        }
+
+        public async Task<User> FindUserById(Guid userId)
+        {
+            return await _dbSet.FirstOrDefaultAsync(user => user.Id == userId);
         }
     }
 }
